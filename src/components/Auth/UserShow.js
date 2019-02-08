12954db6f9a4f8db77ba483e1f8205a5810e00fa
joshuaking06 <joshua.king0692@gmail.com>
2019-02-08 19:51:59 +0000

    added bio and recent posts field on user show page

--- src/components/Auth/UserShow.js
+++ src/components/Auth/UserShow.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import { Segment, Image, Grid, Header, Divider } from 'semantic-ui-react'
+import { Segment, Image, Grid, Header, Divider, Container, Card } from 'semantic-ui-react'
 
 class UserShow extends React.Component{
   constructor(props){
@@ -19,21 +19,41 @@ class UserShow extends React.Component{
     if(!this.state.userData) return null
     const { userData } = this.state
     return(
-      <Segment textAlign='center'>
-        <Image
-          src={userData.image}
-          size='medium'
-          circular
-          centered/>
-        <Divider hidden section/>
+      <div>
+        <Segment inverted color='teal' textAlign='center'>
+          <Divider hidden/>
+          <Image
+            src={userData.image}
+            size='medium'
+            circular
+            centered/>
+          <Divider hidden />
 
-        <Grid columns={1}>
-          <Grid.Column>
-             <Header size='huge'> {userData.first} {userData.last} </Header>
-             <Header as='h3'> {userData.username} </Header>
-          </Grid.Column>
-        </Grid>
+          <Grid columns={1}>
+            <Grid.Column>
+               <Header size='huge'> {userData.first} {userData.last} </Header>
+               <Header as='h3'> {userData.username} </Header>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+
+
+      <Segment style={{ minHeight: 300 }} textAlign='center'>
+        <Divider section hidden />
+        <Container text>
+          <Header as='h3'> Bio </Header>
+          <p>{userData.bio} </p>
+        </Container>
+        <Divider hidden/>
+        <Container>
+          <Header as='h3'> Recent Posts by This User </Header>
+          <Grid columns={3}>
+
+          </Grid>
+        </Container>
+
       </Segment>
+      </div>
     )
   }
 }
