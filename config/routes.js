355548df1eff76063d29fe9a0a56c5c81c046661
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-08 13:32:13 +0000

    Added Register and Login route

--- config/routes.js
+++ config/routes.js
@@ -3,11 +3,19 @@ const router = require('express').Router()
 const continentController = require('../controllers/continents')
 const cityController = require('../controllers/cities')
 const userController = require('../controllers/users')
+// const secureRoute = ('../lib/secureRoute')
+const authController = require('../controllers/auth')
+
+router.post('/register', authController.register)
+router.post('/login', authController.login)
+
 
 router.get('/cities', cityController.index)
+
 router.get('/cities/:id', cityController.show)
 
 router.get('/continents', continentController.index)
+
 router.get('/continents/:id', continentController.show)
 
 router.get('/users', userController.index)
