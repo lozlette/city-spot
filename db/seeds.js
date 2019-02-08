f63d289555ccd6a3eca8f2283644bdaa2a01d940
joshuaking06 <joshua.king0692@gmail.com>
2019-02-08 20:27:48 +0000

    fixed typos and some errors in seeds file, still broken

--- db/seeds.js
+++ db/seeds.js
@@ -46,126 +46,106 @@ mongoose.connect(process.env.MONGODB_URI, (err, db) => {
       return Promise.props({
         city1: City.create({
           name: 'Cape Town',
-          imgae: 'http://www.travelstart.co.za/blog/wp-content/uploads/2018/05/main-best-time-to-visit-cape-town.jpg',
-          continent: 'Africa',
-          appearedIn: [data.continentAfrica]
+          image: 'http://www.travelstart.co.za/blog/wp-content/uploads/2018/05/main-best-time-to-visit-cape-town.jpg',
+          continent: [data.continentAfrica]
         }),
         city2: City.create({
           name: 'Nairobi',
-          imgae: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/06/28/17/singa.jpg',
-          continent: 'Africa',
-          appearedIn: [data.continentAfrica]
+          image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/06/28/17/singa.jpg',
+          continent: [data.continentAfrica]
       }),
       city3: City.create({
         name: 'Accra',
-        imgae: 'https://i1.wp.com/theleidener.com/wp-content/uploads/2017/04/accra1.jpg?ssl=1',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://i1.wp.com/theleidener.com/wp-content/uploads/2017/04/accra1.jpg?ssl=1',
+        continent: [data.continentAfrica]
       }),
       city4: City.create({
         name: 'Libreville',
-        imgae: 'http://www.travelmagma.com/uploads/images/0_319.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'http://www.travelmagma.com/uploads/images/0_319.jpg',
+        continent: [data.continentAfrica]
       }),
       city5: City.create({
         name: 'Johannesburg',
-        imgae: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Johannesburg_view_topofCC_03.jpg/220px-Johannesburg_view_topofCC_03.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Johannesburg_view_topofCC_03.jpg/220px-Johannesburg_view_topofCC_03.jpg',
+        continent: [data.continentAfrica]
       }),
       city6: City.create({
         name: 'Marrakesh',
-        imgae: 'https://timedotcom.files.wordpress.com/2016/03/160314_tra_intl_marrakesh2.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://timedotcom.files.wordpress.com/2016/03/160314_tra_intl_marrakesh2.jpg',
+        continent: [data.continentAfrica]
       }),
       city7: City.create({
         name: 'Cairo',
-        imgae: 'https://frontera.net/wp-content/uploads/2017/01/Egypt-1-1-1-1-1-1-1-2-1.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://frontera.net/wp-content/uploads/2017/01/Egypt-1-1-1-1-1-1-1-2-1.jpg',
+        continent: [data.continentAfrica]
       }),
       city8: City.create({
         name: 'Lagos',
-        imgae: 'https://i1.wp.com/estateintel.com/wp-content/uploads/2017/06/DSC06402.jpg?resize=1000%2C480&ssl=1',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://i1.wp.com/estateintel.com/wp-content/uploads/2017/06/DSC06402.jpg?resize=1000%2C480&ssl=1',
+        continent: [data.continentAfrica]
       }),
       city9: City.create({
         name: 'Giza',
-        imgae: 'https://cdn.civitatis.com/egipto/el-cairo/galeria/giza.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://cdn.civitatis.com/egipto/el-cairo/galeria/giza.jpg',
+        continent: [data.continentAfrica]
       }),
       city10: City.create({
         name: 'Fes',
-        imgae: 'https://c1.staticflickr.com/5/4284/35001088050_52fcd1cb66_b.jpg',
-        continent: 'Africa',
-        appearedIn: [data.continentAfrica]
+        image: 'https://c1.staticflickr.com/5/4284/35001088050_52fcd1cb66_b.jpg',
+        continent: [data.continentAfrica]
       }),
 
 // --------------- EUROPE -----------------
 
         city11: City.create({
           name: 'London',
-          imgae: 'https://zenartdesign.com/wp-content/uploads/2017/03/London-Skyline-800x800px.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://zenartdesign.com/wp-content/uploads/2017/03/London-Skyline-800x800px.jpg',
+          continent: [data.continentEurope]
         }),
         city12: City.create({
           name: 'Berlin',
-          imgae: 'https://blog.parkinn.com/wp-content/uploads/sites/7/2018/01/things-in-berlin-skyline.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://blog.parkinn.com/wp-content/uploads/sites/7/2018/01/things-in-berlin-skyline.jpg',
+          continent: [data.continentEurope]
         }),
         city13: City.create({
           name: 'Rome',
-          imgae: 'http://www.dearomatours.com/wp-content/uploads/alba-di-roma-2.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'http://www.dearomatours.com/wp-content/uploads/alba-di-roma-2.jpg',
+          continent: [data.continentEurope]
         }),
         city14: City.create({
           name: 'Lisbon',
-          imgae: 'https://www.saga.co.uk/contentlibrary/saga/publishing/verticals/travel/city-guides/lisbon.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://www.saga.co.uk/contentlibrary/saga/publishing/verticals/travel/city-guides/lisbon.jpg',
+          continent: [data.continentEurope]
         }),
         city15: City.create({
           name: 'Madrid',
-          imgae: 'https://www.tripsavvy.com/thmb/IFWk4WJn0e-pI59rzr3G1GyK9ZU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/madrid-skyline--gran-v-a-at-dusk-657153546-5a75dd69c5542e00373029b6.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://www.tripsavvy.com/thmb/IFWk4WJn0e-pI59rzr3G1GyK9ZU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/madrid-skyline--gran-v-a-at-dusk-657153546-5a75dd69c5542e00373029b6.jpg',
+          continent: [data.continentEurope]
         }),
         city16: City.create({
           name: 'Paris',
-          imgae: 'https://tunnelingonline.com/wp-content/uploads/2017/07/Paris-skyline.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://tunnelingonline.com/wp-content/uploads/2017/07/Paris-skyline.jpg',
+          continent: [data.continentEurope]
         }),
         city17: City.create({
           name: 'Amsterdam',
-          imgae: 'https://s3.eu-central-1.amazonaws.com/locationscoutnet/images/2018-03/panoramic-view-on-amsterdam-skyline-netherlands_l.jpeg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://s3.eu-central-1.amazonaws.com/locationscoutnet/images/2018-03/panoramic-view-on-amsterdam-skyline-netherlands_l.jpeg',
+          continent: [data.continentEurope]
         }),
         city18: City.create({
           name: 'Athens',
-          imgae: 'https://c1.staticflickr.com/5/4489/36786164663_f66eeaac8b_b.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://c1.staticflickr.com/5/4489/36786164663_f66eeaac8b_b.jpg',
+          continent: [data.continentEurope]
         }),
         city19: City.create({
           name: 'Barcelona',
-          imgae: 'https://www.kimptonhotels.com/blog/wp-content/uploads/2018/09/Barcelona-Skyline.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://www.kimptonhotels.com/blog/wp-content/uploads/2018/09/Barcelona-Skyline.jpg',
+          continent: [data.continentEurope]
         }),
         city20: City.create({
           name: 'Istanbul',
-          imgae: 'https://indigodergisi.com/wp-content/uploads/2016/12/istanbul-gokdelenleri-elektrik-kesintisi-dogalgaz-kaos.jpg',
-          continent: 'Europe',
-          appearedIn: [data.continentEurope]
+          image: 'https://indigodergisi.com/wp-content/uploads/2016/12/istanbul-gokdelenleri-elektrik-kesintisi-dogalgaz-kaos.jpg',
+          continent: [data.continentEurope]
         })
 
       })
