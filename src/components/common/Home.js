584050e4e7541301340c758474e82aad4a910ed2
joshuaking06 <joshua.king0692@gmail.com>
2019-02-08 21:16:59 +0000

    experimented with youtube embedding

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
-
+import  { Embed } from 'semantic-ui-react'
 const Home = () => {
+  const videoId = 'lJ87yD_8u3U'
   return(
-    <h1> Home Page </h1>
+    <div>
+      <h1> Home Page </h1>
+        <Embed id={videoId} placeholder={`https://img.youtube.com/vi/${videoId}/0.jpg`} source='youtube' />
+    </div>
   )
 
 }
