1b4109132e6e6307558f01c3b311c827fc24e727
byemane <beteyemane@hotmail.co.uk>
2019-02-08 11:50:21 +0000

    posts controller & user routes

--- models/user.js
+++ models/user.js
@@ -3,8 +3,23 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  image: {type: String},
-  bio: {type: String, maxlength: 100, required: true},
+  image: { type: String },
+  bio: { type: String, maxlength: 100, required: true },
   password: { type: String, required: true }
 })
+
+userSchema.virtual('posts', {
+  ref: 'Post',
+  localField: '_id',
+  foreignField: 'user'
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform(doc, json) {
+    delete json.__v
+    return json
+  }
+})
+
 module.exports = mongoose.model('User', userSchema)
