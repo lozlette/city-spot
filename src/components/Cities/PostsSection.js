6b117d627a7cdde7bca9c716bf4e6b4eabe2e469
joshuaking06 <joshua.king0692@gmail.com>
2019-02-09 17:29:24 +0000

    set up video on page

---
+++ src/components/Cities/PostsSection.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// {Auth.isAuthenticated() && <Link className="navbar-item" to="/cheeses/new">Add a cheese</Link>}
+
+
+class PostsSection extends React.Component{
+  
+
+  render(){
+    console.log(this.props.city)
+    return(
+      null
+    )
+  }
+}
+
+
+export default PostsSection
