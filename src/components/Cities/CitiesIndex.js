656676b6a766e382856ea3764460dc1a17796e7d
Lauren Bell <laurengilbert0@gmail.com>
2019-02-09 22:52:45 +0000

    cities index page added and auth added to navbar items

---
+++ src/components/Cities/CitiesIndex.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import axios from 'axios'
+
+import  { Header, Grid, Segment } from 'semantic-ui-react'
+class CitiesIndex extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  getStyle(city) {
+    return {
+      width: 300,
+      height: 300,
+      backgroundImage: `url(${city.image})`,
+      backgroundSize: 'cover'
+    }
+  }
+
+  componentDidMount(){
+    axios.get('/api/cities')
+      .then(res => this.setState({ cities: res.data }))
+  }
+
+
+  render (){
+    if(!this.state.cities) return null
+    console.log(this.state.cities)
+    return(
+      <div>
+        <Grid columns={4}>
+          <Grid.Row>
+            {this.state.cities.map(city =>
+              <Grid.Column key={city._id}>
+                <Segment circular id='circle' style={this.getStyle(city)}>
+                  <Header as='h2' className='segmentHeader'>
+                    {city.name}
+                  </Header>
+                </Segment>
+              </Grid.Column>
+            )}
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default CitiesIndex
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Segment, Menu, Dropdown, Button, Modal, Icon } from 'semantic-ui-react'
+import { Segment, Menu, Button, Modal } from 'semantic-ui-react'
 import RegLoginModal from '../Auth/RegLoginModal'
+import Auth from '../../lib/Auth'
 
 
 class Navbar extends React.Component{
@@ -11,45 +12,56 @@ class Navbar extends React.Component{
       activeItem: 'home'
     }
     this.handleItemClick = this.handleItemClick.bind(this)
+    this.handleItemClick2 = this.handleItemClick2.bind(this)
   }
 
   handleItemClick(e, { name }){
     this.setState({ activeItem: name })
+    this.props.history.push('/')
   }
 
+  handleItemClick2(e, { name }){
+    this.setState({ activeItem: name })
+    this.props.history.push('/cities')
+  }
 
   render() {
     const { activeItem } = this.state
-    const cityOptions =  [ { key: '_id', value: '_id', text: 'London' }, {key: '_id2', value: '_id2', text: 'Barcelona'}]
+    // const cityOptions =  [ { key: '_id', value: '_id', text: 'London' }, {key: '_id2', value: '_id2', text: 'Barcelona'}]
 
     return (
       <Segment inverted>
         <Menu inverted pointing secondary>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick} />
 
           <Menu.Item
             name='View All Cities'
             active={activeItem === 'View All Cities'}
-            onClick={this.handleItemClick}
+            onClick={this.handleItemClick2}
           />
 
           <Menu.Menu position='right'>
 
-            <Menu.Item name='Sign Up'>
+            {!Auth.isAuthenticated() && <Menu.Item
+              name='Sign Up'>
               <Modal trigger={<Button inverted>Sign Up</Button>} closeIcon>
                 <Modal.Content>
                   <RegLoginModal tab="Sign Up"/>
                 </Modal.Content>
               </Modal>
-            </Menu.Item>
+            </Menu.Item>}
 
-            <Menu.Item name='Log In'>
+            {!Auth.isAuthenticated() && <Menu.Item
+              name='Log In'>
               <Modal trigger={<Button inverted>Log In</Button>} closeIcon>
                 <Modal.Content>
                   <RegLoginModal tab="Log In" />
                 </Modal.Content>
               </Modal>
-            </Menu.Item>
+            </Menu.Item>}
 
           </Menu.Menu>
         </Menu>
