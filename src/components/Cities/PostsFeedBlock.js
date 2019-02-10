3a0aafeb648afdc75619cf1a28fcbbcab949320a
joshuaking06 <joshua.king0692@gmail.com>
2019-02-10 18:29:32 +0000

    populate the cities one level deeper

--- src/components/Cities/PostsFeedBlock.js
+++ src/components/Cities/PostsFeedBlock.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Feed } from 'semantic-ui-react'
 import ViewPostModal from './ViewPostModal'
+import { Link } from 'react-router-dom'
 
 
 const PostsFeedBlock = ({ text, city, post, index, handleChangePost, handleChangeComment, handleSubmitComment }) => {
+  console.log(post)
   return(
     <Feed.Event>
       <Feed.Label image={post.image} />
       <Feed.Content>
         <Feed.Date>{post.createdAt}</Feed.Date>
         <Feed.Summary>
-          <a>Username</a> created a post about {city.name}.
+          <Link to={`/users/${post.user._id}`}> {post.user.username}</Link> created a post about {city.name}.
         </Feed.Summary>
 
         <ViewPostModal
