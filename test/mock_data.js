efb6b9f3e0241d4a17621d06dae3de29859282fa
joshuaking06 <joshua.king0692@gmail.com>
2019-02-14 16:38:41 +0000

    started test file

--- test/mock_data.js
+++ test/mock_data.js
@@ -11,6 +11,16 @@ const userData = {
   verified: true
 }
 
+const cityData = {
+  name: 'London',
+  image: 'http://www.travelstart.co.za/blog/wp-content/uploads/2018/05/main-best-time-to-visit-cape-town.jpg',
+  videoID: 'sQPXvtJQ9p4',
+  population: '4,524,111',
+  continent: {
+    name: 'Europe',
+  }
+}
+
 module.exports = {
   userData
 }
